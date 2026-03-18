fd912e090c468929a622baf26c604da46e35b771
山口絃 <steven_siro_vai@hotmail.co.jp>
2026-03-18 18:26:48 +0900

    11章まで完了

--- src/test/java/org/example/MoneyTest.java
+++ src/test/java/org/example/MoneyTest.java
@@ -1,12 +1,43 @@
 package org.example;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
     @Test
     public void testMultiplication() {
-        Doller five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+
+//        assertTrue(Money.franc(5).equals(Money.franc(5)));
+//        assertFalse(Money.franc(5).equals(Money.franc(6)));
+
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+//    @Test
+//    public void testFrancMultiplication() {
+//        Money five = Money.franc(5);
+//        assertEquals(Money.franc(10), five.times(2));
+//        assertEquals(Money.franc(15), five.times(3));
+//    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
+//    @Test
+//    public void testDifferentClassEquality() {
+//        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+//    }
 }
